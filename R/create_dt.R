3662346e9159e066cf4cedd7756afd327ab2248c
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-01-17 04:48:01 +0000

    pass up buttons arg [skip ci]

--- R/create_dt.R
+++ R/create_dt.R
@@ -4,26 +4,29 @@
 #' @param dat Data to show.
 #' @param caption Table caption.
 #' @param scrollY Maximum height (in pixels) before defaulting to scrolling.
+#' @param buttons Download button types to include in table.
 #'
 #' @family general
 #' @export
 #' @examples
 #' create_dt(dat = mtcars)
 create_dt <- function(dat,
-                      caption="",
-                      scrollY=400){
-  requireNamespace("DT")
-    data <- DT::datatable(
-      data = dat,
-      caption = caption,
-      extensions = 'Buttons',
-      options = list( dom = 'Bfrtip',
+                      caption = "",
                       buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
-                      scrollY = scrollY,
-                      scrollX=TRUE, scrollCollapse = TRUE, paging = FALSE,
-                      columnDefs = list(list(className = 'dt-center',
-                                             targets = "_all"))
-      )
+                      scrollY = 400){
+  requireNamespace("DT")
+  DT::datatable(
+    data = dat,
+    caption = caption,
+    extensions = 'Buttons',
+    options = list( dom = 'Bfrtip',
+                    buttons = buttons,
+                    scrollY = scrollY,
+                    scrollX=TRUE,
+                    scrollCollapse = TRUE,
+                    paging = FALSE,
+                    columnDefs = list(list(className = 'dt-center',
+                                           targets = "_all"))
     )
-    return(data)
+  )
 }
