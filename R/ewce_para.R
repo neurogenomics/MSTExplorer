0cbd55d193be2789ccd0e55ae19b006105eebe48
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-11-07 15:59:34 +0000

    Fix ewce_para example

--- R/ewce_para.R
+++ R/ewce_para.R
@@ -28,7 +28,7 @@
 #' @examples
 #' gene_data <- HPOExplorer::load_phenotype_to_genes()
 #' ctd <- MultiEWCE::load_example_ctd()
-#' list_names <- unique(gene_data$hpo_name)[seq(3)]
+#' list_names <- unique(gene_data$hpo_id)[seq(3)]
 #' res_files <- ewce_para(ctd = ctd,
 #'                        gene_data = gene_data,
 #'                        list_names = list_names,
